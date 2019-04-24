7edf456923c212eb058b5a9347ba7f749f49d9de
Leyda <48352107+leydaaltun@users.noreply.github.com>
2019-04-24 19:03:00 +0300

    js

---
+++ allEmails.js
@@ -0,0 +1,55 @@
+let allMails = [
+    {
+        date: '23 January 2019',
+        from: 'Susanne' ,
+        subject: 'About holiday at July' , 
+        mail: 'Dear Leyda,as you probably know...' ,
+        isFavorited: true,
+    },
+    {
+        date: '22 January 2019',
+        from: 'Zeit Now' ,
+        subject: 'Email confirmation' , 
+        mail: 'Please confrim your email for 20th time..' ,
+        isFavorited: true,
+    },
+    {
+        date: '19 January 2019',
+        from: 'Facebook' ,
+        subject: 'New message' , 
+        mail: 'You got a message from a random per..' ,
+        isFavorited: true,
+    },
+    {
+        date: '18 January 2019',
+        from: 'Github' ,
+        subject: 'Forgot your password' , 
+        mail: 'Click on this link to reset your passwor..' ,
+        isFavorited: true,
+    },
+    {
+        date: '15 January 2019',
+        from: 'Roboard' ,
+        subject: 'Bender sent you a DM' , 
+        mail: 'KILL ALL HUMANS' ,
+        isFavorited: true,
+    },
+    {
+        date: '11 January 2019',
+        from: 'SuperOffer' ,
+        subject: 'Earn $1000 from home' , 
+        mail: 'CLICK HERE TO GET YOUR REWARD' ,
+        isFavorited: true,
+    },
+    {
+        date: '10 January 2019',
+        from: 'YSK',
+        subject: 'Your e-mazbata is here',
+        mail: 'Dear Leyda,to get your mazbata...',
+        isFavorited: false,
+    }
+]
+
+function mail(date,from,subject,mail,isFavorited) {
+
+}
